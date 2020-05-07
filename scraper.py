634baa4170a2d009377efd75cc408d4890c66161
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-05-07 15:16:34 -0400

    remove newline from tokens

--- scraper.py
+++ scraper.py
@@ -15,9 +15,11 @@ def main():
     # NOTE: tweet id (incrementing) is found in tweet url, advanced search is helpful for looking back
     start_id = 1212375795538309120  # tweet id at least start_id
     final_id = 1256030193325662208  # tweet id at most final_id
-    new_tweets = api.user_timeline(screen_name=target_username, since_id=start_id, max_id=final_id, count=1)
+    new_tweets = api.user_timeline(screen_name=target_username, since_id=start_id, max_id=final_id, count=1,
+                                   tweet_mode='extended')
     tweets = new_tweets
     final_id = tweets[-1].id - 1  # update final_id to be less than the oldest tweet collected (last in list)
+    print(tweets)
     while len(new_tweets) > 0:  # until we have retrieved all tweets
         # return all tweets in range, 100 at a time
         new_tweets = api.user_timeline(screen_name=target_username, since_id=start_id, max_id=final_id, count=100)
@@ -33,15 +35,16 @@ def main():
     data_temp['Favourites'] = np.array([tweet.favorite_count for tweet in tweets])
     data_temp['RTs'] = np.array([tweet.retweet_count for tweet in tweets])
 
+
     data_temp.to_csv(out_file, index=False, encoding='utf-8')
 
 
 def get_tokens():
     f = open('shhh.txt', 'r')
     f.readline()
-    consumer_key = f.readline()
-    consumer_secret = f.readline()
-    access_token = f.readline()
+    consumer_key = f.readline().rstrip("\n")
+    consumer_secret = f.readline().rstrip("\n")
+    access_token = f.readline().rstrip("\n")
     access_secret = f.readline()
     f.close()
     return consumer_key, consumer_secret, access_token, access_secret
