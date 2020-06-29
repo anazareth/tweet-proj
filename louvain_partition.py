71ec41a898a6a0ef59dadfdc30f112eb171bee5e
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-06-29 16:55:39 -0400

    remove f>3, doesn't do what i thought

--- louvain_partition.py
+++ louvain_partition.py
@@ -46,7 +46,7 @@ def find_communities(network, month, username):
         num_partitions = max(partition.values()) + 1  # communities labelled 0 to k-1, where k is number of communities
         iter_tracking[num_partitions] += 1  # increment counter
     # f is frequency of partition; N/3 is set threshold; don't want less than 4 communities
-    close_values = [f for f in iter_tracking.values() if f > N / 3 and f > 3]
+    close_values = [f for f in iter_tracking.values() if f > N / 3]
     # if it's close, take the lower number of communities
     num_communities = min([k for k in iter_tracking.keys() for f in close_values if f == iter_tracking[k]])
     pct_certain = iter_tracking.get(num_communities)/N
