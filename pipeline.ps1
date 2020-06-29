ab112be98729c8c75515afb72b73328103d7621b
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-06-28 22:43:55 -0400

    correct stopwords file

--- pipeline.ps1
+++ pipeline.ps1
@@ -3,7 +3,7 @@ python scraper.py $username 1212268323691532289 1267229979449966594
 Write-Output '--------------------------------------------------------------------------------------'
 python clean_tweets.py data\$username'_raw.csv' 1 $username
 Write-Output '--------------------------------------------------------------------------------------'
-python keyword_analysis.py data\$username'_clean.csv' meta\blankisms.txt meta\nostopwords.txt $username
+python keyword_analysis.py data\$username'_clean.csv' meta\blankisms.txt meta\stopwords.txt $username
 Write-Output '--------------------------------------------------------------------------------------'
 python adj_matrices.py data\kw_ana\$username'_tokenized.csv' data\kw_ana\$username'_words.csv' $username
 Write-Output '--------------------------------------------------------------------------------------'
