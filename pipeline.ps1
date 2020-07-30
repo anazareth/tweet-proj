c0cf1ac88b09e950f86de886f461f491a143bb7a
Alex Nazareth <31732666+anazareth@users.noreply.github.com>
2020-07-30 13:26:46 -0400

    add scraper exception for random file

--- pipeline.ps1
+++ pipeline.ps1
@@ -2,7 +2,7 @@ $username = $args[0]
 Write-Output '--------------------------------------------------------------------------------------'
 Write-Output "Beginning processing for $username"
 Write-Output '--------------------------------------------------------------------------------------'
-if ( $username -ne 'realDonaldTrump' -and $username -notlike '*sample*' ) {
+if ( $username -ne 'realDonaldTrump' -and $username -notlike '*sample*' -and $username -notlike '*random*' ) {
 	# scraper doesn't work for donald trump, but everything else works
 	python scraper.py $username 1212268323691532289 1278078666942173184
 }
